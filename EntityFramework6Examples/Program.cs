37314d0880293f1baad710f22ab98a67426b6781
Eric Stallcup <StallcupE@gmail.com>
2014-06-05 13:39:12 -0400

    initial commit

---
+++ EntityFramework6Examples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechSmith.Interact.DataAccess.SalesDataAccess;
+
+namespace EntityFramework6Examples
+{
+   class Program
+   {
+      static void Main( string[] args )
+      {
+         Console.WriteLine( "Welcome to Entity Framework 6 Examples!\n--------------------------------------\n\n" );
+
+         RunQueries();
+
+         Console.WriteLine( "At Console.ReadLine()\n" );
+         Console.ReadLine();
+      }
+
+      public async static void RunQueries()
+      {
+         using ( var context = new SalesContext() )
+         using ( var asyncExamples = new SalesAsyncExamples( context ) )
+         {
+            Console.WriteLine( "Executing GetNumberOfOrdersAsync()\n" );
+            await asyncExamples.GetNumberOfOrdersAsync().ContinueWith( t => WriteToConsole( t.Result + "\n\n" ) );
+
+            Console.WriteLine( "Executing GetCountriesAsync()\n" );
+            await asyncExamples.GetCountriesAsync().ContinueWith( t => WriteCountryCollectionToCommandPrompt( t.Result ) );
+         }
+      }
+
+      private static void WriteToConsole<T>( T result )
+      {
+         Console.WriteLine( result );
+      }
+
+      private static void WriteCountryCollectionToCommandPrompt( IEnumerable<Country> countryCollection )
+      {
+         foreach ( var c in countryCollection.Take( 10 ) )
+         {
+            Console.WriteLine( "ISO Code: {0} - Name: {1}", c.CountryIsoCode, c.CountryPrintName );
+         }
+      }
+   }
+}
---
+++ EntityFramework6Examples/SalesAsyncExamples.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using TechSmith.Interact.DataAccess.SalesDataAccess;
+
+namespace EntityFramework6Examples
+{
+   public class SalesAsyncExamples : IDisposable
+   {
+      private readonly ISalesContext _salesContext;
+
+      public SalesAsyncExamples()
+         : this( new SalesContext() )
+      {
+      }
+
+      public SalesAsyncExamples( ISalesContext salesContext )
+      {
+         if ( salesContext == null )
+            throw new ArgumentNullException( "salesContext" );
+
+         _salesContext = salesContext;
+      }
+
+      public async Task<IEnumerable<Country>> GetCountriesAsync()
+      {
+         var countries = await ( from c in _salesContext.Countries
+                                 where c.CountryActive
+                                 select c ).ToListAsync();
+
+
+         return countries;
+      }
+
+      public async Task<int> GetNumberOfOrdersAsync()
+      {
+         var count = await ( from o in _salesContext.Ords
+                             select o ).CountAsync();
+
+         return count;
+      }
+
+      public void Dispose()
+      {
+         _salesContext.Dispose();
+         GC.SuppressFinalize( this );
+      }
+   }
+}
